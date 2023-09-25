629ce637c65c1863e081796ec3ec0b22c588655a
cezevogue <cezdesaulle.evogue@gmail.com>
2023-09-25 09:31:12 +0200

    media link ok

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -2,21 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Media;
+use App\Form\MediaType;
 use App\Repository\MediaTypeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MediaController extends AbstractController
 {
-    #[Route('/media', name: 'media_type_choice')]
+    #[Route('/', name: 'media_type_choice')]
     public function index(MediaTypeRepository $repository): Response
     {
         $media_types=$repository->findAll();
 
 
         return $this->render('media/index.html.twig', [
-          'media_types'=> $media_types
+            'media_types'=> $media_types
+        ]);
+    }
+
+
+
+
+    #[Route('/create/{id}/{type}', name: 'media_create')]
+    public function create(Request $request,EntityManagerInterface $manager, MediaTypeRepository $repository, $id, $type): Response
+    {
+
+        $media=new Media();
+
+        $media_type=$repository->find($id);
+        $media->setType($media_type);
+
+        $form=$this->createForm(MediaType::class, $media);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($media);
+            $manager->flush();
+
+            $this->addFlash('success', 'Média créé');
+            return $this->redirectToRoute('media_type_choice');
+        }
+
+        return $this->render('media/create.html.twig', [
+            'form'=>$form->createView()
         ]);
     }
 }
---
+++ src/Form/MediaType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Media;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required'=>false,
+                'label'=>'Liens de la vidéo youTube',
+                'attr'=>[
+
+                    'placeholder'=>'Collez lez liens de la vidéo YouTube souhaitée'
+                ]
+
+            ])
+            ->add('Valider', SubmitType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Media::class,
+        ]);
+    }
+}
