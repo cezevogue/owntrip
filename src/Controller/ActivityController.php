bf3057d8a702bf2a7aea78f5424cf26ac2048581
cezevogue <cezdesaulle.evogue@gmail.com>
2023-09-27 09:03:04 +0200

    media link ok

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Activity;
+use App\Entity\Media;
 use App\Form\ActivityType;
+use App\Form\MediaType;
 use App\Repository\ActivityRepository;
+use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,44 +20,116 @@ class ActivityController extends AbstractController
     #[Route('/create', name: 'activity_create')]
     public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        $activity=new Activity();
+        $activity = new Activity();
 
-        $form=$this->createForm(ActivityType::class, $activity);
+        $form = $this->createForm(ActivityType::class, $activity);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $manager->persist($activity);
             $manager->flush();
 
-            return $this->redirectToRoute('media_type_choice', ['param'=>'activity', 'id'=>$activity->getId() ]);
+            return $this->redirectToRoute('media_type_choice', ['param' => 'activity', 'id' => $activity->getId()]);
+
+        }
+
+
+        return $this->render('activity/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/list', name: 'activity_list')]
+    public function activity_list(ActivityRepository $activityRepository): Response
+    {
+        $activities = $activityRepository->findAll();
+
+        // dd($activities[2]->getMedias());
+
+
+        return $this->render('activity/activity_list.html.twig', [
+            'activities' => $activities
+        ]);
+    }
+
+    #[Route('/update/infos/{id}', name: 'activity_update_infos')]
+    public function update_infos(Request $request, Activity $activity, EntityManagerInterface $manager): Response
+    {
+
+
+
+        $form = $this->createForm(ActivityType::class, $activity);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($activity);
+            $manager->flush();
+
+            return $this->redirectToRoute('activity_list');
 
         }
 
 
 
-
-
-        return $this->render('activity/index.html.twig', [
+        return $this->render('activity/activity_update_infos.html.twig', [
            'form'=>$form->createView()
         ]);
     }
 
-         #[Route('/list', name: 'activity_list')]
-          public function activity_list(ActivityRepository $activityRepository): Response
+    #[Route('/update/medias/{id}', name: 'activity_update_medias')]
+    public function update_medias(ActivityRepository $activityRepository, MediaRepository $mediaRepository, EntityManagerInterface $manager,Request $request , $id): Response
+    {
+
+        $activity=$activityRepository->find($id);
+        $medias=$mediaRepository->findBy(['activity'=>$activity]);
+
+        return $this->render('activity/activity_update_medias.html.twig', [
+           'medias'=>$medias,
+            'id'=>$id
+        ]);
+    }
+
+
+    #[Route('/delete/{id}', name: 'activity_delete')]
+    public function delete(ActivityRepository $activityRepository, MediaRepository $mediaRepository, EntityManagerInterface $manager, $id): Response
+    {
+          $activity=$activityRepository->find($id);
+          $medias=$mediaRepository->findBy(['activity'=>$activity]);
+
+          foreach ($medias as $media)
           {
-              $activities=$activityRepository->findAll();
+
+              if ($media->getType()->getName()!='Lien')
+              {
+                  unlink($this->getParameter('upload_dir').'/'.$media->getName());
+
+              }
+
+              $activity->removeMedia($media);
 
 
-
-              return $this->render('activity/activity_list.html.twig', [
-                 'activities'=>$activities
-              ]);
           }
+          $manager->remove($activity);
+          $manager->flush();
 
 
+
+        return $this->redirectToRoute('activity_list');
+    }
+
+    #[Route('/details/{id}', name: 'activity_details')]
+    public function details(Activity $activity): Response
+    {
+
+
+        return $this->render('activity/activity_details.html.twig', [
+            'activity'=>$activity
+        ]);
+    }
 
 
 }
