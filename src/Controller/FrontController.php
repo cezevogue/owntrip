dfe5d7667c2a96ea78c08a1aa5ec7edbea7f9397
cezevogue <cezdesaulle.evogue@gmail.com>
2023-09-29 11:02:09 +0200

    filtres

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ActivityRepository;
 use App\Repository\ActivityTypeRepository;
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,38 +23,60 @@ class FrontController extends AbstractController
 
     #[Route('/activities/result', name: 'activities_result')]
     #[Route('/activities/filter', name: 'activities_filter')]
-    public function activities_result(ActivityRepository $activityRepository,ActivityTypeRepository $typeRepository, Request $request): Response
+    #[Route('/activities/{city}', name: 'activities_redir')]
+    public function activities_result(ActivityRepository $activityRepository, ActivityTypeRepository $typeRepository, Request $request, CartService $cartService, $city = null): Response
     {
-        $city=$request->request->get('city');
-        $types=$typeRepository->findAll();
+        if (!$city) {
 
-        if ($request->request->has('type')){
-           $activities=array();
-            foreach($request->request->all()['type'] as $type){
+            $city = $request->request->get('city');
+        }
 
-                $results=$activityRepository->findByCityAndType($city, $type);
-                foreach ($results as $result){
-                    $activities[]=$result;
+        $types = $typeRepository->findAll();
+
+        if ($request->request->has('type')) {
+            $activities = array();
+            foreach ($request->request->all()['type'] as $type) {
+
+                $results = $activityRepository->findByCityAndType($city, $type);
+                foreach ($results as $result) {
+                    $activities[] = $result;
                 }
 
             }
 
-        }else{
+        } else {
 
-            $activities=$activityRepository->findByCity($city);
+            $activities = $activityRepository->findByCity($city);
 
         }
 
+        $wishes = $cartService->getCartWithData();
 
-       //dd($activities);
-
+        //dd($wishes);
 
 
         return $this->render('front/activities_result.html.twig', [
-            'activities'=>$activities,
-            'types'=>$types,
-            'city'=>$city
+            'activities' => $activities,
+            'types' => $types,
+            'city' => $city,
+            'wishes' => $wishes
         ]);
+    }
+
+    #[Route('/add/wish/{id}/{city}', name: 'add_wish')]
+    public function add_wish(CartService $cartService, $id, $city): Response
+    {
+        $cartService->add($id);
+
+        return $this->redirectToRoute('activities_redir', ['city' => $city]);
+    }
+
+    #[Route('/remove/wish/{id}/{city}', name: 'remove_wish')]
+    public function remove_wish(CartService $cartService, $id, $city): Response
+    {
+        $cartService->remove($id);
+
+        return $this->redirectToRoute('activities_redir', ['city' => $city]);
     }
 
 
