5f4e4edb6f953a9c701e31cce57482ed3fa969d5
cezevogue <cezdesaulle.evogue@gmail.com>
2023-10-02 15:28:57 +0200

    avec stripe pret pour deploy

--- src/Controller/StripePayController.php
+++ src/Controller/StripePayController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Service\CartService;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,7 +24,7 @@ class StripePayController extends AbstractController
           {
             $line_items[]=[
                 'price_data'=>[
-                    'unit_amount'=>$item['activity']->getPrice(), 'currency'=>'EUR',
+                    'unit_amount'=>$item['activity']->getPrice()*100, 'currency'=>'EUR',
                     'product_data'=>['name'=>$item['activity']->getName()
                     ]
                 ],
@@ -33,16 +35,46 @@ class StripePayController extends AbstractController
           }
 
 
+         Stripe::setApiKey('sk_test_51NwOTlJ2Zd0dLxMHVyJSgNydBGye36Q96U200SAMIOxPBtTlK3UopVhKAsKHJrNFovwXw6Za2Qb4b4zq9x5NymCt00mw0P8R3y');
+             $session=Session::create([
+                 'success_url'=>'https://www.lock.cezdigit.com/commande/success',
+                 'cancel_url'=>'https://www.lock.cezdigit.com/wishList',
+                // 'success_url'=>'http://127.0.0.1:8000/commande/success',
+                // 'cancel_url'=>'http://127.0.0.1:8000/wishList',
+                 'payment_method_types'=>['card'],
+                 'line_items'=>$line_items,
+                 'mode'=>'payment'
+             ]);
+
+             return $this->redirect($session->url, 303);
 
 
 
-
-
-
-
-
-        return $this->render('stripe_pay/index.html.twig', [
-            'controller_name' => 'StripePayController',
-        ]);
     }
+
+
+
+      #[Route('/commande/{success}', name: 'commande')]
+          public function commande(CartService $cartService,$success=null): Response
+          {
+             if ($success){
+
+                 $this->addFlash('success', 'Merci pour votre confiance');
+                 $cartService->destroy();
+                 return $this->redirectToRoute('app_front');
+
+             }else{
+                 $this->addFlash('danger', 'Un problème est survenu merci de réitérer votre paiement');
+                 return $this->redirectToRoute('app_front');
+
+             }
+
+
+          }
+
+
+
+
+
+
 }
